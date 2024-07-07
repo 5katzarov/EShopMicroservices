a04162bf1be1a75babb01d55ede7a78f38809261
5ko <PKatzarovCareer@gmail.com>
2024-07-07 09:35:14 -0400

    Added cross-cutting concerns to the catalog api

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,6 +1,12 @@
 ﻿global using Carter;
 global using Mapster;
+global using Marten;
 global using MediatR;
 global using BuildingBlocks.CQRS;
+global using Catalog.API.Data;
 global using Catalog.API.Models;
-global using Marten;
+global using Catalog.API.Exceptions;
+global using FluentValidation;
+global using BuildingBlocks.Exceptions.Handler;
+global using BuildingBlocks.Behaviors;
+global using Marten.Pagination;
